203eb9b24cf5a62501312d1a21f60da1e9746b0b
Nikita Lvov <niklvov@gmail.com>
2015-03-06 14:59:59 +0300

    start with top menu

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: [
+            "build/**/*.*"
+        ],
+        copy: {
+            font: {
+                expand: true,
+                cwd: 'source/font/',
+                src: '**',
+                dest: 'build/font/'
+            },
+            img: {
+                expand: true,
+                cwd: 'source/img/',
+                src: '**',
+                dest: 'build/img/'
+            }
+        },
+        concat: {
+            js: {
+                src: [
+                    'source/bower/jquery/jquery.js',
+                    'source/bower/jquery.scrollTo/jquery.scrollTo.js',
+                    'source/bower/jquery.serialScroll/jquery.serialScroll.js',
+                    'source/bower/jquery-parallax/scripts/jquery.parallax-1.1.3.js',
+                    'source/bower/slicknav/jquery.slicknav.js',
+                    'source/js/app.js'
+                ],
+                dest: 'build/js/app.js'
+            }
+        },
+        less: {
+            css: {
+                files: {'build/css/app.css': 'source/css/app.less'}
+            }
+        },
+        jade: {
+            index: {
+                options: {
+                    pretty: true
+                },
+                files: {'build/index.html': 'source/index.jade'}
+            }
+        },
+        watch: {
+            js: {
+                files: ['source/js/app.js'],
+                tasks: ['concat:js']
+            },
+            less: {
+                files: ['source/css/app.less'],
+                tasks: ['less:css']
+            },
+            jade: {
+                files: ['source/index.jade'],
+                tasks: ['jade:index']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['clean', 'copy', 'jade', 'less', 'concat']);
+
+};
--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,6 @@
-
+$(function () {
+    $('#menu').slicknav({
+        label: ''
+    });
+    $('.slicknav_menu').prepend('<div class="logo"><img src="img/logo.png"/></div>');
+});
