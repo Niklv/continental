2c921b8a9d519d991f41c80b44236ef1e594258c
Tezaruz <danilalvov@gmail.com>
2015-03-07 00:59:28 +0300

    yandex map

--- source/js/app.js
+++ source/js/app.js
@@ -6,3 +6,35 @@ $(function () {
     });
     $('.slicknav_menu').prepend('<div class="logo"><a href="#"><img src="img/logo.png"/></a></div>');
 });
+
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+        console.log(position);
+    }
+);
+
+ymaps.ready(function () {
+
+    var myMap = new ymaps.Map('map', {
+        center: [55.74, 37.58],
+        zoom: 13,
+        controls: []
+    });
+
+
+    var geolocation = ymaps.geolocation
+    myMap.behaviors.disable('scrollZoom');
+    myMap.controls.add('zoomControl')
+    geolocation.get({
+        provider: 'yandex',
+        mapStateAutoApply: true
+    }).then(function (result) {
+// Красным цветом пометим положение, вычисленное через ip.
+        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+        result.geoObjects.get(0).properties.set({
+            balloonContentBody: 'Мое местоположение'
+        });
+//myMap.geoObjects.add(result.geoObjects);
+    });
+    myMap.controls.add(searchControl);
+});
